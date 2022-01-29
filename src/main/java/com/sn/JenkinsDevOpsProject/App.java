11b5c9c46d40ece634651a712961a3120571ae26
sunil.yerubandi <sunil.yerubandi@servicenow.com>
2022-01-30 02:57:54 +0530

    Update Pom file

--- src/main/java/com/sn/JenkinsDevOpsProject/App.java
+++ src/main/java/com/sn/JenkinsDevOpsProject/App.java
@@ -7,8 +7,45 @@ package com.sn.JenkinsDevOpsProject;
 public class App {
     public static void main( String[] args )	{
     	
-       System.out.println("STRY0010039: Story update");
-       System.out.println("STRY0010040: Story update");
+    	App obj = new App();
+    	
+    	System.out.println("a + b + c = "+ obj.add("Service","Now", "DevOps"));
+       
+    	System.out.println("a + b = "+ obj.add(5,6));
+    	System.out.println("a - b = "+ obj.subtact(5,6));
+    	System.out.println("a * b = "+ obj.multiply(5,6));
+    	System.out.println("a / b = "+ obj.division(30,6));
+  
     }
+    
+    public int add(int a, int b) {
+    	
+    	return a+b;
+    	
+    }
+    
+    public String add(String a, String b, String c) {
+    	
+    	return a+" "+b+" "+c;
+    	
+    }
+    
+    public int subtact(int a, int b) {
+    	
+    	return a-b;
+    	
+    }
+
+   public int multiply(int a, int b) {
+    	
+    	return a*b;
+    	
+    }
+   
+   public float division(int a, int b) {
+   	
+   	return a/b;
+   	
+   }
 
 }
--- src/main/java/com/sn/JenkinsDevOpsProject/JenkinsIT.java
+++ src/main/java/com/sn/JenkinsDevOpsProject/JenkinsIT.java
@@ -8,11 +8,6 @@ public class JenkinsIT {
     public static void main( String[] args )	{
     	
        System.out.println("STRY0010039: Story update");
-       System.out.println("STRY0010045: Story update");
-       System.out.println("STRY0010090: Story update");
-       System.out.println("STRY0010099: Adding new Junit test for newly added methods");
-       System.out.println("RAJU-20: Story update");
-       System.out.println("RAJU-18: Story update");
     }
 
 }
